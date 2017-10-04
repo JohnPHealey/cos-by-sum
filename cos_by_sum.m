5614909bfc6cac01a4328ff1233bb1329774e890
JohnPHealey <jph432@mun.ca>
2017-10-04 16:17:34 -0230

    Calculate factorials semi-recursively

--- cos_by_sum.m
+++ cos_by_sum.m
@@ -3,11 +3,15 @@ function [double_res, single_res] = cos_by_sum(x)
     x = pi/4 * (x-1);
     curr_term = 0;
     double_res = 0;
+    denom = 2
     for n = 1:50
-        display(n)
-        temp = (-1)^n * x^(2*n) / factorial(2*n);
+        %display(n)
+        if n > 1
+            denom = 2*n*(2*n-1)*denom;
+        end;
+        temp = (-1)^n * x^(2*n) / denom;
         double_res = double_res + temp;
-        if (abs(temp - curr_term) < 0.000001)
+        if (abs(temp - curr_term) < 0.00001)
             break;
         end;
         curr_term = temp;
